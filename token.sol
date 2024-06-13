283a781b289463ba26f8b00a354997a4dbf0ce4d
Dakshhhhhhhhhhhh <kumardaksh3214@gmail.com>
2024-06-13 10:04:49 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -18,8 +18,8 @@ pragma solidity 0.8.18;
 contract MyToken {
 
     // public variables here
-    string public tokenName = "LINKED";
-    string public tokenAbbrv = "LINK";
+    string public tokenName = "INFORMAGTION";
+    string public tokenAbbrv = "INFO";
     uint public totalSupply = 0;
 
     // mapping variable here
